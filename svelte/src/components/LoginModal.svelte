761488597e3021126914019550cb34791234b1e6
John Norris <john@norricorp.f9.co.uk>
2023-07-11 07:18:17 +0100

    migration to Svelte 4

--- svelte/src/components/LoginModal.svelte
+++ svelte/src/components/LoginModal.svelte
@@ -82,7 +82,7 @@
 <!-- Modal -->
 {#if open}
   <div class="modal" id="sampleModal" tabindex="-1" role="dialog" aria-labelledby="sampleModalLabel" aria-hidden={false}>
-    <div class="modal-dialog" role="document" in:fly={{ y: -50, duration: 300 }} out:fly={{ y: -50, duration: 300, easing: quintOut }}>
+    <div class="modal-dialog" role="document" in:fly|global={{ y: -50, duration: 300 }} out:fly|global={{ y: -50, duration: 300, easing: quintOut }}>
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="sampleModalLabel">Login / Forgot Password</h5>
@@ -114,7 +114,7 @@
     </div>
   </div>
   {#if showBackdrop}
-    <div class="modal-backdrop show" transition:fade={{ duration: 150 }} />
+    <div class="modal-backdrop show" transition:fade|global={{ duration: 150 }} />
   {/if}
 {/if}
 
